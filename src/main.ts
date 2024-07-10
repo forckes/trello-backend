495e6e4678271857ce4138d2ad513790c935dd53
forckes <forckes@proton.me>
2024-07-10 13:27:43 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as cookiesParser from 'cookie-parser'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+
 	app.setGlobalPrefix('api')
-	app.enableCors()
+
+	app.use(cookiesParser())
+
+	app.enableCors({
+		origin: ['http://localhost:3001'],
+		credentials: true,
+		exposedHeaders: 'set-cookie'
+	})
+
 	await app.listen(process.env.PORT || 4200)
 }
 bootstrap()
