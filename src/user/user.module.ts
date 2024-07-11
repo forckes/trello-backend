96007a690c9494d6cbfe8754513b8f02925b6a7d
forckes <forckes@proton.me>
2024-07-11 19:00:08 +0300

    f

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
+import { Module } from '@nestjs/common'
+import { UserService } from './user.service'
+import { UserController } from './user.controller'
+import { PrismaService } from 'src/prisma.service'
 
 @Module({
-  controllers: [UserController],
-  providers: [UserService],
+	imports: [PrismaService],
+	controllers: [UserController],
+	providers: [UserService]
 })
 export class UserModule {}
